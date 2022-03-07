b53bb55fa48eeb4b6967a78b418bd0c2a32ed645
Szabó Zétény <szaboz20@ADY-NAGYATAD.SULINET.HU>
2022-03-07 08:43:35 +0100

    5. fe

--- otszaz/otszaz.py
+++ otszaz/otszaz.py
@@ -1,6 +1,24 @@
-print("1. feladat:")
 f = open("penztar.txt", "r")
-
-print("2. feladat:")
+kosar = []
+#szoveg = f.read()
+#print(szoveg)
+for sor in f:
+    kosar.append(sor.strip())
 
 f.close()
+
+print("A fizetések száma: " + str(kosar.count("F")))
+
+print("Az első vásárló " + str(kosar.index("F")) + " darab árucikket vásárolt.")
+
+sorszam = int(input("Vásárlás sorszáma: "))
+arunev = input("Árucikk neve: ")
+darab = int(input("Darabszám: "))
+
+aruIndex = kosar.index(arunev)
+darabLista = kosar[:aruIndex]
+print(darabLista)
+vasarlasDb = darabLista.count("F") + 1
+print(vasarlasDb)
+
+print("Az első vásárlás sorszáma: " + str(vasarlasDb))
