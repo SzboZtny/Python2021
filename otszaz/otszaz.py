d86c79bc2d0ffdb8555c32f438ed016825f0d882
Szabó Zétény <szaboz20@ADY-NAGYATAD.SULINET.HU>
2022-03-07 11:26:58 +0100

    5. feladat

--- otszaz/otszaz.py
+++ otszaz/otszaz.py
@@ -8,10 +8,8 @@ for sor in f:
 
 f.close()
 
-print("2. feladat:")
 print("A fizetések száma: " + str(kosar.count("F")))
 
-print("3. feladat:")
 print("Az első vásárló " + str(kosar.append("F")) + " darab árucikket vásárolt.")
 
 print("A fizetések száma: " + str(kosar.count("F")))
@@ -22,10 +20,27 @@ sorszam = int(input("Vásárlás sorszáma: "))
 arunev = input("Árucikk neve: ")
 darab = int(input("Darabszám: "))
 
+#A termék első indexe
 aruIndex = kosar.index(arunev)
 darabLista = kosar[:aruIndex]
-print(darabLista)
+#print(darabLista)
 vasarlasDb = darabLista.count("F") + 1
-print(vasarlasDb)
+#print(vasarlasDb)
 
 print("Az első vásárlás sorszáma: " + str(vasarlasDb))
+
+#A termék utolsó indexe
+utolsoIndex = 0
+for i in range(0, len(kosar)):
+    if kosar[i * -1 - 1] == arunev:
+        utolsoIndex = len(kosar)-i
+        break
+darabLista = kosar[:utolsoIndex]
+vasarlasDb = darabLista.count("F") + 1
+print("Az utolsó vásárlás sorszáma: " + str(vasarlasDb))
+
+voltF = False
+szam = 0
+for e in kosar:
+    if e == aruev:
+        
