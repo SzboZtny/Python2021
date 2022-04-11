4b362e16645134e5e778ad495b65e7103d65e9d6
Szabó Zétény <szaboz20@ADY-NAGYATAD.SULINET.HU>
2022-04-11 12:35:42 +0200

    .

---
+++ oop1.py
@@ -0,0 +1,17 @@
+class Kutya:
+    def __init__(self, nev):
+        self.nev = nev
+
+    def ugat():
+        print("VAU-VAU (" + self.nev + ".")
+
+egyKutya = Kutya("Armageddon")
+print(egyKutya.nev)
+
+
+
+
+
+
+
+
