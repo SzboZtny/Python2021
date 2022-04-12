bb33431fd83ef06e7e5866ce2d01055f6fd66930
SzboZtny <szabo.zeteny.20@ady-nagyatad.hu>
2022-04-12 08:31:56 +0200

    .

--- oop1.py
+++ oop1.py
@@ -1,17 +1,34 @@
+#Osztály definíció
+
 class Kutya:
+    #Konstruktor
     def __init__(self, nev):
         self.nev = nev
+    #Osztály függvény
+    def ugat(self):
+        print("VAU-VAU(" + self.nev + ")")
 
-    def ugat():
-        print("VAU-VAU (" + self.nev + ".")
-
+#Példányosítás
 egyKutya = Kutya("Armageddon")
 print(egyKutya.nev)
+egyKutya.ugat()
 
+#Osztályváltozó értékadása
+egyKutya.nev = "Bruno"
+print(egyKutya.nev)
 
+#Öröklés, leszármaztatás
+class NemetJuhasz(Kutya):
+    #Új függvény
+    def pitiz(self):
+        print(self.nev + ": Nein!")
+    #Függvény felülírás
+    def ugat(self):
+        print("WAU-WAU")
 
+n =  NemetJuhasz("Rex")
+n.ugat()
+n.pitiz()
 
-
-
-
-
+n2 = NemetJuhasz("Kondér")
+n2.pitiz()
