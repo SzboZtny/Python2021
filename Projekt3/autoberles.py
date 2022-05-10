5d9322b46c5c46bec719b1ac8372480bfb4f7c53
SzboZtny <szabo.zeteny.20@ady-nagyatad.hu>
2022-05-10 08:44:07 +0200

    Projekt

---
+++ Projekt3/autoberles.py
@@ -0,0 +1,51 @@
+menu_options = {
+    1: "Autók listája és adatai",
+    2: "Bérlési árak",
+    3: "Szabályzat",
+    4: "Információk a cégről",
+    5: "Kilépés",
+}
+
+def menu1():
+    print(menu_options[1])
+
+def menu2():
+    print(menu_options[2])
+
+def menu3():
+    print(menu_options[3])
+
+def menu4():
+    print(menu_options[4])
+
+def print_menu():
+    #print(menu_options)
+    for menupontIndex in menu_options:
+        minta = "{} --- {}"
+        print(minta.format(menupontIndex, menu_options[menupontIndex]))
+        #print(menu_options[menupontIndex])
+    print("============================================")
+    pass
+
+print("================ Menüpontok ================")
+
+while (True):
+    print_menu()
+    try:
+        option = int(input("Válassz: "))
+    except:
+        option = "nincsilyen"
+        pass
+
+    if option == 1:
+        menu1()
+    elif option == 2:
+        menu2()
+    elif option == 3:
+        menu3()
+    elif option == 4:
+        menu4()
+    elif option == 5:
+        break
+    else:
+        print("Nincs ilyen menüpont!")
