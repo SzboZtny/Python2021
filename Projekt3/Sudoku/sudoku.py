ca71e1de0a203b0ce33e8319486f8053d586dcc9
Szabó Zétény <szaboz20@ADY-NAGYATAD.SULINET.HU>
2022-05-18 09:39:37 +0200

    Sudoku

--- Projekt3/Sudoku/sudoku.py
+++ Projekt3/Sudoku/sudoku.py
@@ -1,11 +1,12 @@
+import math
 
 print("1. feladat:")
 #filenev = input("Adja meg a bemeneti fájl nevét! ")
 #sor = int(input("Adja meg egy sor számát! "))
 #oszlop = int(input("Adja meg egy oszlop számát! "))
 filenev = "nehez.txt"
-sor = 1
-oszlop = 1
+sor = 7
+oszlop = 7
 
 f = open(filenev)
 sorok = f.read().split("\n")[:-1]
@@ -32,5 +33,23 @@ lepesek = adatok[9:]
 #print(lepesek)
 
 print("3. feladat:")
+if tabla[sor-1][oszlop-1] == 0:
+    print("Az adott helyet még nem töltötték ki.")
+else:
+    print("Az adott helyen szereplő szám: " + str(tabla[sor-1][oszlop-1]))
 
-print("Az adott helyen szereplő szám: " + str(tabla[sor-1][oszlop-1]))
+s = math.ceil(sor / 3) - 1
+o = math.ceil(oszlop / 3)
+
+print("A hely a(z) {} résztáblázathoz tartozik.".format(s * 3 + o))
+
+db = 0
+for s in tabla:
+    print(s)
+    db += s.count("0")
+
+print("4. feladat:")
+print("Az üres helyek aránya: {:.0%}".format(db / 81))
+
+for lepes in lepesek:
+    print(lepes)
