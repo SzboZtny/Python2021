95c1a5b1c773dc5a3df4595726e4092e001baa2f
Szabó Zétény <szaboz20@ADY-NAGYATAD.SULINET.HU>
2022-05-30 12:35:05 +0200

    SMS

--- SMS/sms.py
+++ SMS/sms.py
@@ -4,7 +4,7 @@ f = open("sms.txt")
 sorok = f.read().split("\n")[1:-1]
 f.close()
 
-print(sorok)
+#print(sorok)
 
 smsek = []
 for i in range(0,len(sorok)//2):
@@ -33,3 +33,64 @@ print("óra: {}, perc: {}, telefonszám: {}, üzenet: {}".format(smsek[minIndex]
                                                               smsek[minIndex].perc,
                                                               smsek[minIndex].telefonszam,
                                                               smsek[minIndex].uzenet))
+
+print("4. feladat:")
+stat = [0, 0, 0, 0, 0]
+for a in smsek:
+    if len(a.uzenet) <= 20:
+        stat[0] += 1
+    elif len(a.uzenet) <= 40:
+        stat[1] += 1
+    elif len(a.uzenet) <= 60:
+        stat[2] += 1
+    elif len(a.uzenet) <= 80:
+        stat[3] += 1
+    else:
+        stat[4] += 1
+
+print(stat)
+
+stat = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+stat = []
+for i in range(0, 24):
+    stat.append(0)
+
+for a in smsek:
+    stat[a.ora] += 1
+
+ossz = 0
+for a in stat:
+    if a > 10:
+        ossz += a-10
+print("5. feladat: Ernőnek {} sms-t kell fizetnie".format(ossz))
+
+
+print("6. feladat:")
+elozo = -1
+idotav = []
+for a in smsek:
+    if a.telefonszam == "123456789":
+        if elozo != -1:
+            idotav.append(a.idoperc()-elozo)
+            elozo = a.idoperc()
+print(idotav)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
